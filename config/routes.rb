d571dba7004bae41a7d7b479977673a4de18f66d
Frank Tennant <frank.tennant@wmg.com>
2023-09-05 12:35:55 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    root "articles#index"
+    resources :articles
+  end
+  # Defines the root path route  ("/")
+  # root "articles#index"
+end
