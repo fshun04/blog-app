24a2b96103d5c4e8cd0beaa0b54fd2edfae1dcc5
Frank Tennant <frank.tennant@wmg.com>
2023-09-07 10:22:40 +0100

    base webpage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
     root "articles#index"
-    resources :articles
+    resources :articles do
+      resources :comments 
+    end
   end
   # Defines the root path route  ("/")
   # root "articles#index"
